552f962451e60369879dc3ad8e4da6c1dd47d9b6
SynthCyraX <SynthCyraX@gmail.com>
2023-03-03 11:05:34 +0100

    Added operator-functions and boilerplate in html

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const addition = function(a, b){
+    return a + b;
+}
+const subtraction = function(a, b){
+    return a - b;
+}
+const multiplication = function(a, b){
+    return a * b;
+}
+const division = function(a, b){
+    return a / b;
+}
+
+const operate = function(){
+    
+};
+console.log(addition(5,6));
+console.log(subtraction(5, 6));
+console.log(multiplication(5, 6));
+console.log(division(5,6));
