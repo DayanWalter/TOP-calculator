30c0424b0e4c3a1fc0e343a8ec43d031662c33ca
SynthCyraX <SynthCyraX@gmail.com>
2023-03-04 09:57:44 +0100

    Added class=key

--- javascript.js
+++ javascript.js
@@ -12,18 +12,22 @@ const division = function(a, b){
 }
 
 const operate = function(a, operator, b){
-if(operator === "+"){
+    const plus = "+";
+    const minus = "-";
+    const multi = "*";
+    const divi = "/"
+if(operator === plus){
     return addition(a, b);
-}else if(operator === "-"){
+}else if(operator === minus){
     return subtraction(a, b);
-}else if(operator === "*"){
+}else if(operator === multi){
     return multiplication(a, b)
-}else if(operator === "/"){
+}else if(operator === divi){
     return division(a, b);
 }
 
 };
-console.log(addition(5,6));
-console.log(subtraction(5, 6));
-console.log(multiplication(5, 6));
-console.log(division(5,6));
+console.log(addition(7,7));
+console.log(subtraction(7, 7));
+console.log(multiplication(7, 7));
+console.log(division(7,7));
